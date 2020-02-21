7bb118ce2cdb92a4326e40f52e5843f4510a781d
Platinum-wang <765945069@qq.com>
2020-02-21 21:37:18 +0800

    day 1

---
+++ Information.java
@@ -0,0 +1,8 @@
+public class Information {
+    public static void main(String[] args){
+        System.out.println("姓名：张三");
+        System.out.println("性别：男");
+        System.out.println("籍贯：北京");
+        System.out.println("住址：北京市XXXXXX");
+    }
+}
---
+++ LoveJava.java
@@ -0,0 +1,13 @@
+public class LoveJava {
+    public static void main(String[] args){
+        System.out.println("\t*\t\t\t\t\t\t\t\t\t\t\t\t*");
+        System.out.println("*\t\t\t*\t\t\tI love Java\t\t\t*\t\t\t*");
+        System.out.println("\t*\t\t\t\t\t\t\t\t\t\t\t\t*");
+        System.out.println("\t\t*\t\t\t\t\t\t\t\t\t\t*");
+        System.out.println("\t\t\t*\t\t\t\t\t\t\t\t*");
+        System.out.println("\t\t\t\t*\t\t\t\t\t\t*");
+        System.out.println("\t\t\t\t\t*\t\t\t\t*");
+        System.out.println("\t\t\t\t\t\t*\t\t*");
+        System.out.println("\t\t\t\t\t\t\t*");
+    }
+}
